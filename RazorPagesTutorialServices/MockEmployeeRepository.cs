b52ee2de779c506d07432697982e20eb89e06ebb
Ali Chameh <Ali.S.Chameh@gmail.com>
2021-10-28 21:52:45 +0330

    Add MockEmployeeRepository

---
+++ RazorPagesTutorialServices/MockEmployeeRepository.cs
@@ -0,0 +1,29 @@
+﻿using RazorPagesTutorialModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RazorPagesTutorialServices
+{
+    public class MockEmployeeRepository : IEmployeeRepository
+    {
+        public MockEmployeeRepository()
+        {
+            _employeeList = new List<Employee>()
+            {
+                new Employee(){Id = 1, Name = "Mary",Department= Department.HR,Email ="mary@par.com",PhotoPath = "mary.png"},
+                new Employee(){Id = 2, Name = "John",Department= Department.IT,Email ="john@par.com",PhotoPath = "john.png"},
+                new Employee(){Id = 3, Name = "Sara",Department= Department.IT,Email ="Sara@par.com",PhotoPath = "Sara.png"},
+                new Employee(){Id = 4, Name = "Dave",Department= Department.Payrol,Email ="David@par.com",PhotoPath = "David.png"}
+            };
+        }
+
+        private List<Employee> _employeeList { get; set; }
+        public IEnumerable<Employee> GetAllEmployees()
+        {
+            return _employeeList;
+        }
+    }
+}
