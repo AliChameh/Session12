6e601338d57f1285ad07c7fa0637a0814868efae
Ali Chameh <Ali.S.Chameh@gmail.com>
2021-10-28 21:20:53 +0330

    Add GetAllEmployee() to IEmployeeRepository

--- RazorPagesTutorialServices/IEmployeeRepository.cs
+++ RazorPagesTutorialServices/IEmployeeRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RazorPagesTutorialModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,6 @@ namespace RazorPagesTutorialServices
 {
     interface IEmployeeRepository
     {
+        IEnumerable<Employee> GetAllEmployees();
     }
 }
