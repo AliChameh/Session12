75451d813c11bab10ca3947b2bb3e106fe40a126
Ali Chameh <Ali.S.Chameh@gmail.com>
2021-10-29 08:05:19 +0330

    correct image extension

--- RazorPagesTutorialServices/MockEmployeeRepository.cs
+++ RazorPagesTutorialServices/MockEmployeeRepository.cs
@@ -13,10 +13,10 @@ namespace RazorPagesTutorialServices
         {
             _employeeList = new List<Employee>()
             {
-                new Employee(){Id = 1, Name = "Mary",Department= Department.HR,Email ="mary@par.com",PhotoPath = "mary.png"},
-                new Employee(){Id = 2, Name = "John",Department= Department.IT,Email ="john@par.com",PhotoPath = "john.png"},
-                new Employee(){Id = 3, Name = "Sara",Department= Department.IT,Email ="Sara@par.com",PhotoPath = "Sara.png"},
-                new Employee(){Id = 4, Name = "Dave",Department= Department.Payrol,Email ="David@par.com",PhotoPath = "David.png"}
+                new Employee(){Id = 1, Name = "Mary",Department= Department.HR,Email ="mary@par.com",PhotoPath = "mary.jpg"},
+                new Employee(){Id = 2, Name = "John",Department= Department.IT,Email ="john@par.com",PhotoPath = "john.jpg"},
+                new Employee(){Id = 3, Name = "Sara",Department= Department.IT,Email ="Sara@par.com",PhotoPath = "Sara.jpg"},
+                new Employee(){Id = 4, Name = "Dave",Department= Department.Payrol,Email ="David@par.com"}
             };
         }
 
